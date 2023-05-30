82f1cd1bc97653d0dadf3beb6d2e80b5b9a3bd9f
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2023-05-30 17:53:48 -0300

    Navegando nas paginas atraves de routerLinks
    Modulo 8 - Cadastro e Listagem de Clientes.

--- clientes-app/src/app/clientes/clientes-routing.module.ts
+++ clientes-app/src/app/clientes/clientes-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientesFormComponent } from './clientes-form/clientes-form.component'
 
-
-const routes: Routes = [];
+//aqui nos vamos definir a rota para as TELAS conforme os LINKS
+//q estao na SIDEBAR
+const routes: Routes = [
+{path: 'clientes-form' , component: ClientesFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

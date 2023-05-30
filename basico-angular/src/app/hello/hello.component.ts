ee121a3ec9ae0144ad00e7627cb27c6fccce0273
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2023-05-30 16:39:33 -0300

    Templates e Interpolação
    Módulo 6 - Módulo de introdução ao Angular.

--- basico-angular/src/app/hello/hello.component.ts
+++ basico-angular/src/app/hello/hello.component.ts
@@ -1,13 +1,32 @@
+
 import { Component } from '@angular/core'
 
 @Component({
     selector: 'hello',
-    template: `
-        <h1>Hello World</h1>
-    `
+    templateUrl: './hello.component.html'
 })
 export class HelloComponent {
 
+ 
+    nome: string;
 
+    clientes: Cliente[]
 
+    constructor() {
+        this.nome = 'Fulano';
+        let fulano = new Cliente('Fulano', 30);
+        let cicrano = new Cliente ('Cicrano', 25);
+
+        let outro = new Cliente('Outro', 32);
+		
+    this.clientes = [fulano, cicrano, outro];
+    }
+}
+
+class Cliente {
+    constructor(
+
+        public nome: string,
+        public idade: number
+    ){}
 }
